9dcfbae7e6a07d9afefaf859b69d513d388f97b6
Issa Jubril <issa.jubril@andela.com>
2016-01-03 12:53:24 +0100

    [Feature #110488142] Set up Facebook Authentication pipeline

--- djangogram/static/js/script.js
+++ djangogram/static/js/script.js
@@ -4,11 +4,9 @@ var image_content = "";
 
 $(document).ready(function() {
 $('.single-image').click(function(){
-   var image_to_be_edited = $(this).find('img').attr('src');
+var image_to_be_edited = $(this).find('img').attr('src');
 $('#img-effect').find('img').attr('src', image_to_be_edited);
 });
 
 
 });
-
-
