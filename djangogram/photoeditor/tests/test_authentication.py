2d889fdd992abd7c4310ed241775b0f64ae3e8c2
Issa Jubril <issa.jubril@andela.com>
2016-01-14 16:58:01 +0100

    [Feature #110549302] Modified test

--- djangogram/photoeditor/tests/test_authentication.py
+++ djangogram/photoeditor/tests/test_authentication.py
@@ -5,7 +5,7 @@ from django.test import TestCase, Client
 class AuthenticationTestCase(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user()
+        self.user = User.objects.create_user(username='awesome', password='tested')
 
     def test_user_can_login(self):
         self.assertEqual(2 + 2, 4)
