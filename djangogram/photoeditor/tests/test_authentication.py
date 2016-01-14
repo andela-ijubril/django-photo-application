edf399ef99757987da0cf6d539a937dca9727899
Issa Jubril <issa.jubril@andela.com>
2016-01-14 16:49:36 +0100

    [Feature #110549302] Add facebook id

--- djangogram/photoeditor/tests/test_authentication.py
+++ djangogram/photoeditor/tests/test_authentication.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 
 
 class AuthenticationTestCase(TestCase):
 
     def setUp(self):
-        pass
+        self.user = User.objects.create_user()
 
     def test_user_can_login(self):
         self.assertEqual(2 + 2, 4)
+
+    def test_authenticated_routes(self):
+        pass
